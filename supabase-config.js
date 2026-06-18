52fc11e2eb8e7a0833b1b622ec561ba3852e2a0e
jbsccg-creator <jb.sccg@gmail.com>
2026-06-18 18:12:15 +0200

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,8 @@
 const SUPABASE_URL = "https://jxdcmkdsbdompvyoqjbp.supabase.co";
+
 const SUPABASE_ANON_KEY = "sb_publishable_rgVAuatgi9YljulaOTLbZA_rhKOS0Ar";
 
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
